2511198ba5ee39dbd0709d02711d834abeb7b6ad
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-07-25 14:37:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,8 @@ const proxy = require('./src/proxy')
 
 const PORT = process.env.PORT || 8080
 
-app.disable('trust proxy')
+app.disable("x-powered-by");
+app.disable('trust proxy');
 app.get('/', authenticate, params, proxy)
 app.get('/favicon.ico', (req, res) => res.status(204).end())
 app.listen(PORT, () => console.log(`Worker ${process.pid}: Listening on ${PORT}`))
